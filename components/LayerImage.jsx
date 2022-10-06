a0ac9a700507bbe0243ef1989590338ac1cf7e52
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2022-10-06 16:59:57 +0200

    Image layer rendering should no longer fail when no image is present. Bugfix: image layers that should have hidden overflow did not.

--- components/LayerImage.jsx
+++ components/LayerImage.jsx
@@ -3,24 +3,25 @@
 export default function LayerImage({ data, panelNr, host }) {
   const { image, altText, layerId, id, overflow, basePosition, debug, fit, forceLoad } = data;
 
-  const source = host+image._path
+  const source = host+image?._path
+
 
   return (
+    <div className={`overflowImageWrapper  ${overflow ? "showOverflow" : "hideOverflow"}`}>
     <div
       id={layerId}
-      className={`layerImage ${fit || ''} ${basePosition || 'center-center'} ${debug ? "debug" : ""} ${
-        overflow ? "showOverflow" : "hideOverflow"
-      }`}
+      className={`layerImage ${fit || ''} ${basePosition || 'center-center'} ${debug ? "debug" : ""} ${overflow ? "showOverflow" : "hideOverflow"}`}
     >
       <img
         id={id}
         loading={(panelNr === 0 || forceLoad) ? "eager" : "lazy"}
         className="image"
-        width={image.width}
-        height={image.height}
+        width={image?.width}
+        height={image?.height}
         src={source}
         alt={altText?.plaintext}
       />
     </div>
-  );
+    </div>
+  )
 }
