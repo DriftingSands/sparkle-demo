082c99451c60b351f16bae817bcbf782ec09531b
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2022-10-14 14:07:07 +0200

    refactored getData function to make it more readable

--- components/utils.js
+++ components/utils.js
@@ -34,24 +34,26 @@ const tryFetch = async (AEMHeadless, host, endpoint, variation, setState, isAuth
 export const getData = async (variation, setStates, hostConfig, authorHost, publishHost, endpoint, AEMHeadless) => {
   const { setData, setIsAuthorVersion, setFetchError, setCustomHost } = setStates;
   // tryFetch() will return a truthy value if the fetch is successful
-  let successfulFetch = null;
-  // try author host if it's present
-  if (authorHost) {
-    setCustomHost(authorHost);
-    // apply fetch result
-    successfulFetch = await tryFetch(AEMHeadless, authorHost, endpoint, variation, setData, true);
+  let fetchWasSuccessful = null;
+
+  const arr = [
+    {host: authorHost, isAuthor: true},
+    {host: publishHost, isAuthor: false},
+  ]
+
+  for (let i = 0; i < arr.length; i++) {
+    const {host, isAuthor} = arr[i]
+    setCustomHost(host)
+    fetchWasSuccessful = await tryFetch(AEMHeadless, host, endpoint, variation, setData, isAuthor)
+    if (fetchWasSuccessful) {break;}
   }
-  // if the author fetch fails and an publish host is present, try publish host
-  if (publishHost && !successfulFetch) {
-    setCustomHost(publishHost);
-    successfulFetch = await tryFetch(AEMHeadless, publishHost, endpoint, variation, setData, false);
-  }
-  // if no fetch was successful (never returned anything other then false) set error state
-  if (successfulFetch === false) {
+
+  // if no fetch was successful, set error state
+  if (fetchWasSuccessful === false) {
     setFetchError({ type: "publish", url: hostConfig.publishPath });
   }
   // if the author host was successful change the state to render the page in author view
-  if (successfulFetch === "author") {
+  if (fetchWasSuccessful === "author") {
     setIsAuthorVersion(true);
   }
 };
