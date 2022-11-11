42ce72a62da59eaf7682f5f6d04d105fef5dcc28
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2022-11-11 16:49:45 +0100

    adjusting menus for SEO score

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,13 +1,13 @@
 import { scrollToId } from "./utils";
 
 export default function Menu({ menuItems, activeMenuItem }) {
-  const onClickHandler = link => {
+  const onClickHandler = (link) => {
     if (link) {
       scrollToId(link);
     }
     // add hash to url without refreshing page
     window.history.replaceState(window.location.href.split("#")[0], null, link);
-    window.postMessage({type: 'hashUpdate', hash: link}, window.location.origin)
+    window.postMessage({ type: "hashUpdate", hash: link }, window.location.origin);
   };
 
   return (
@@ -15,18 +15,14 @@ export default function Menu({ menuItems, activeMenuItem }) {
       <ul className="menuList">
         {menuItems.map((item, index) => {
           return (
-            <a
+            <li
               key={index}
-              href={!item.active ? (!item?.link?.startsWith("#") ? item.link : null) : null}
               onClick={() => onClickHandler(item?.link)}
+              className={`menuListItem ${activeMenuItem === item.menuItemId ? "active" : ""}`}
+              id={"menuItem-" + item.menuItemId}
             >
-              <li
-                className={`menuListItem ${activeMenuItem === item.menuItemId ? "active" : ""}`}
-                id={"menuItem-" + item.menuItemId}
-              >
-                {item.text}
-              </li>
-            </a>
+              {item.text}
+            </li>
           );
         })}
       </ul>
