afe9f0993159df26180b75b6826b11c757f2a40a
Patrick Zimmermann <patrickmzimmermann@gmail.com>
2023-03-20 17:18:37 +0100

    prettier

--- components/Editable.jsx
+++ components/Editable.jsx
@@ -2,15 +2,15 @@ import { useEffect, useCallback, useRef } from "react";
 
 function editable(WrappedComponent) {
   // next js useRouter doesn't seem to work in higher order components
-  const searchParams = new URLSearchParams(typeof window !== 'undefined' ? window.location.search : '') 
+  const searchParams = new URLSearchParams(typeof window !== "undefined" ? window.location.search : "");
   // eslint-disable-next-line react/display-name
   return props => {
-    if (searchParams.get('editMode') !== "true") {
-      return <WrappedComponent {...props} />
+    if (searchParams.get("editMode") !== "true") {
+      return <WrappedComponent {...props} />;
     }
 
-    const path = props.path
-    
+    const path = props.path;
+
     const editableRef = useRef(null);
 
     const handleScrollMessage = useCallback(
@@ -38,9 +38,9 @@ function editable(WrappedComponent) {
       }
     }, [handleScrollMessage, path]);
     // const path = props?.path
-    console.log("\x1b[31m ~ path:", path)
-    return <WrappedComponent {...props} data-editable-path={path} editableRef={editableRef} />
-  }
+    console.log("\x1b[31m ~ path:", path);
+    return <WrappedComponent {...props} data-editable-path={path} editableRef={editableRef} />;
+  };
 }
 
 export default editable;
